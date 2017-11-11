890b8ec83371cc790915a2389abcb7e3dbc64404
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-11 20:19:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
 }
 
 function deepestChild() {
